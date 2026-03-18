650193f7b8cab3a82c774e8e05ab2fa25c94070f
nonneof1 <fresh.1of@gmail.com>
2026-03-18 22:43:14 +0400

    sdelal

---
+++ 3prakfull/3.2/3.2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _3._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool A = true, B = false, C = true;
+
+            Console.WriteLine(!(A || (!B && C)));
+            Console.WriteLine(A && !(B && A || !C));
+            Console.WriteLine(!(!A || B && C));
+            Console.WriteLine(!(A || !B && C) || C);
+            Console.WriteLine(!(A && !B || C) && B);
+            Console.WriteLine(!(!A || B && C) || A);
+        }
+    }
+}
