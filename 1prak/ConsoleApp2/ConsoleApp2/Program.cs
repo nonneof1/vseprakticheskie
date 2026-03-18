2ed17ae67e8d69674576d799e74d9d734e1751b0
nonneof1 <fresh.1of@gmail.com>
2026-03-18 21:03:49 +0400

    1prak

---
+++ 1prak/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Работа 1";
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("C# — это современный, \n\tс открытым исходным кодом, \n\t\tобъектно-ориентированный \n\t\t\tязык программирования”.");
+
+            Console.Write("Нажми любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1prak/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Работа 1 Проект 2."; //огласление программы
+            Console.WindowWidth = 60; //размер окна
+            Console.WindowHeight = 13; //размер окна
+            Console.BackgroundColor = ConsoleColor.Blue; //цвет
+            Console.Clear();
+            Console.SetCursorPosition(15, 6); //позиция курсора
+            Console.WriteLine("Напишите свое имя: ");
+            string Name = Console.ReadLine();
+            //Console.Write("Привет! ");
+            //Console.WriteLine(Name);
+            Console.WriteLine("Привет {0}!", Name);
+            Console.Write("Для завершения нажмите любую из клавиш...");
+            Console.ReadKey();
+        }
+    }
+}
